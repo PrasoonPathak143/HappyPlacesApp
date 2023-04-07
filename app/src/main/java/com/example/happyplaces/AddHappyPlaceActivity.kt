765081ca26027d7ebc820bdaac177d043b19664f
Prasoon <pp209738@gmail.com>
2023-04-07 15:45:53 +0530

    First Commit

---
+++ app/src/main/java/com/example/happyplaces/AddHappyPlaceActivity.kt
@@ -0,0 +1,11 @@
+package com.example.happyplaces
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AddHappyPlaceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_happy_place)
+    }
+}
